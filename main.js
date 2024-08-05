5d17e302a21a9d542928d84e33031570a56de663
samar31akhtar <samar45akhtar@gmail.com>
2024-08-05 23:51:26 +0530

    Add files via upload

---
+++ main.js
@@ -0,0 +1,7 @@
+var typed = new Typed(".text", {
+    strings: ["Front-End Developer" ,  "Back-End Developer"  ,  "Full Stack Web Developer"],
+    typeSpeed: 100,
+    backSpeed: 100,
+    backDelay: 500,
+    loop:true
+}); 
